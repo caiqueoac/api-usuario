af6acc51b97c74de1f823d1fe2d35870a55cee41
Caiqueoac <caiqueoc@gmail.com>
2021-06-15 17:15:15 -0300

    Criar API com get, post, delete e update

---
+++ api-usuario/Controllers/V1/UserController.cs
@@ -0,0 +1,96 @@
+﻿using api_usuario.Data;
+using api_usuario.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_usuario.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        /*private DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }*/
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<User>>> Get([FromServices] DataContext context)
+        {
+            var users  = await context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<User>> Post(
+            [FromServices] DataContext context,
+            [FromBody]User model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete([FromServices] DataContext context, Guid id )
+        {
+            var item = await context.Users.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            context.Users.Remove(item);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Put([FromServices] DataContext context, Guid id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(user).State = EntityState.Modified;
+
+            
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return NotFound("User not found!");
+            }
+
+               
+          
+            return NoContent();
+        }
+
+    }
+}
---
+++ api-usuario/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using api_usuario.Controllers;
+
+namespace api_usuario.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "Exceeded 60 character limit")]
+        [MinLength(6, ErrorMessage = "Requires at least 6 characters")]
+        public string Name { get; set; }
+
+        //TODO: Validar senha forte
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
